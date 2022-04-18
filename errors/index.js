e84110268a11661f6532429b0aea7470a0bedc70
Ifaldzi <ifaldzialwihudhori@gmail.com>
2022-04-18 07:54:05 +0700

    add endpoint for check token

--- errors/index.js
+++ errors/index.js
@@ -1,9 +1,11 @@
 const CustomError = require("./CustomError")
 const ForbiddenError = require("./ForbiddenError")
 const UnauthorizedError = require('./UnauthorizedError')
+const BadRequestError = require('./BadRequestError')
 
 module.exports = {
     CustomError,
     ForbiddenError,
-    UnauthorizedError
+    UnauthorizedError,
+    BadRequestError
 }
